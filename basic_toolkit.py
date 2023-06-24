ee373106164067cffca8f8d514ec0a6eab2ffb34
Shane Whelan <41593034+SpW82@users.noreply.github.com>
2023-06-24 23:42:15 +0100

    Update basic_toolkit.py
    Updated for Windows compatibility.

--- basic_toolkit.py
+++ basic_toolkit.py
@@ -9,6 +9,7 @@ from ssh_cex import ssh_cex_run
 from ftp_anon import ftp_anon_run
 from mac_finder import *
 from pscanm import ps_s3_run
+import platform
 import os
 
 def run_app():
@@ -20,32 +21,39 @@ def run_app():
 			color_yellow('*. ctrl-c_to_exit\n'),
 			color_blue('\n====== '), color_red('Toolkit_modules'), color_blue(' ======\n'))
 
+			clr = 'clear'
+			sys_info = platform.uname()
+
+			if sys_info.system == 'Windows':
+				clr = 'cls'
+
 			choice = input(color_blue('[*] Enter module number: '))
 			print ('\n')
+
 			if choice == '1':
-				os.system('clear')
+				os.system(clr)
 				ps_s3_run()
-				os.system('clear')
+				os.system(clr)
 			elif choice == '2':
-				os.system('clear')
+				os.system(clr)
 				bg1_run()
-				os.system('clear')
+				os.system(clr)
 			elif choice == '3':
-				os.system('clear')
+				os.system(clr)
 				ssh_cex_run()
-				os.system('clear')
+				os.system(clr)
 			elif choice == '4':
-				os.system('clear')
+				os.system(clr)
 				ftp_anon_run()
-				os.system('clear')
+				os.system(clr)
 			elif choice == '5':
-				os.system('clear')
+				os.system(clr)
 				multi_hash()
-				os.system('clear')
+				os.system(clr)
 			elif choice == '6':
-				os.system('clear')
+				os.system(clr)
 				mac_finder_run()
-				os.system('clear')
+				os.system(clr)
 			else:
 				print (color_red('[*] Invalid Input'))
 
@@ -55,4 +63,3 @@ def run_app():
 
 if __name__ == '__main__':
 	run_app()
-
